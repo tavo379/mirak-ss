3e5964f3e12d03dd38aabe74aadb27a2879e5cf6
DESKTOP-B6QH5CA\tavor <tavoramirez379@gmail.com>
2017-08-04 19:10:46 -0500

    actualizado frontend sliders

--- src/pages/admin-sliders.js
+++ src/pages/admin-sliders.js
@@ -58,15 +58,8 @@ export default class SliderForm extends Component{
                           }}
                         /><span> Max. 30 caracteres</span>
                         </div>
-                        <span className="inputs-title">subtítulo</span><br/>
-                        <div className="inputs-group">
-                        <input
-                          type="text"
-                          onChange={res => {
-                            this.setState({ subtitulo: res.target.value });
-                          }}
-                        /><span> Max. 30 caracteres</span>
-                        </div>
+
+
                         <span className="inputs-title">Link del slider</span><br/>
                         <div className="inputs-group">
                         <input
@@ -81,7 +74,154 @@ export default class SliderForm extends Component{
                     <div className="col-md-4">
                       <div className="new-item-anuncio">
                         <h2>Imagen slider</h2>
-                        <button>Seleccionar archivo</button>
+                        <input
+                          type="file"
+                          onChange={res => {
+                            this.handleImageChange(res);
+                          }}
+                        />
+                        <span className="size-description">El tamaño debe ser de 1280 x 580px</span>
+                      </div>
+                    </div>
+                    <div className="col-md-8">
+                      <span className="number-anuncio">2</span>
+                      <div className="inputs">
+                        <span className="inputs-title">Título</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ titulo: res.target.value });
+                          }}
+                        /><span> Max. 30 caracteres</span>
+                        </div>
+
+
+                        <span className="inputs-title">Link del slider</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ sliderlink: res.target.value });
+                          }}
+                        />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="new-item-anuncio">
+                        <h2>Imagen slider</h2>
+                        <input
+                          type="file"
+                          onChange={res => {
+                            this.handleImageChange(res);
+                          }}
+                        />
+                        <span className="size-description">El tamaño debe ser de 1280 x 580px</span>
+                      </div>
+                    </div>
+                    <div className="col-md-8">
+                      <span className="number-anuncio">3</span>
+                      <div className="inputs">
+                        <span className="inputs-title">Título</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ titulo: res.target.value });
+                          }}
+                        /><span> Max. 30 caracteres</span>
+                        </div>
+
+
+                        <span className="inputs-title">Link del slider</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ sliderlink: res.target.value });
+                          }}
+                        />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="new-item-anuncio">
+                        <h2>Imagen slider</h2>
+                        <input
+                          type="file"
+                          onChange={res => {
+                            this.handleImageChange(res);
+                          }}
+                        />
+                        <span className="size-description">El tamaño debe ser de 1280 x 580px</span>
+                      </div>
+                    </div>
+                    <div className="col-md-8">
+                      <span className="number-anuncio">4</span>
+                      <div className="inputs">
+                        <span className="inputs-title">Título</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ titulo: res.target.value });
+                          }}
+                        /><span> Max. 30 caracteres</span>
+                        </div>
+
+
+                        <span className="inputs-title">Link del slider</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ sliderlink: res.target.value });
+                          }}
+                        />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="new-item-anuncio">
+                        <h2>Imagen slider</h2>
+                        <input
+                          type="file"
+                          onChange={res => {
+                            this.handleImageChange(res);
+                          }}
+                        />
+                        <span className="size-description">El tamaño debe ser de 1280 x 580px</span>
+                      </div>
+                    </div>
+                    <div className="col-md-8">
+                      <span className="number-anuncio">5</span>
+                      <div className="inputs">
+                        <span className="inputs-title">Título</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ titulo: res.target.value });
+                          }}
+                        /><span> Max. 30 caracteres</span>
+                        </div>
+
+
+                        <span className="inputs-title">Link del slider</span><br/>
+                        <div className="inputs-group">
+                        <input
+                          type="text"
+                          onChange={res => {
+                            this.setState({ sliderlink: res.target.value });
+                          }}
+                        />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="new-item-anuncio">
+                        <h2>Imagen slider</h2>
                         <input
                           type="file"
                           onChange={res => {
